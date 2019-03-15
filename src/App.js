4d76588651681a9f10bee7c6f22d11765c67e7bd
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-03-15 13:53:09 +0100

    jarvis web app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chatbot from 'react-simple-chatbot';
 
+const steps = [
+  {
+    id: '0',
+    message: 'Hi my name is  Jarvis',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    message: 'What is  your name',
+    trigger: '2',
+  },
+  {
+    id: '2',
+    user:true,
+    trigger:'3'
+  },
+  {
+    id: '3',
+    message: ' hi {previousValue}, nice to meet you',
+    trigger :'4'
+  },
+  {
+    id:"4",
+    message:"How ,can i help you",
+    trigger:'5'
+  },
+  {
+    id:'5',
+    options:[
+      {value:1, label:'help',trigger:'0'},
+    ]
+  }
+
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Chatbot 
+         speechSynthesis={{ enable: true, lang: 'en' }}
+            recognitionEnable={true}
+            headerTitle="Jarvis"   steps={steps}/>
       </div>
     );
   }
